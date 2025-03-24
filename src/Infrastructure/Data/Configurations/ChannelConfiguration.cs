fcb14fb2efe3726626dbf3c4734309b0a3263795
Ines CHAOUCH <chaouchines00@gmail.com>
2025-03-24 21:46:08 +0100

    Initial commit

---
+++ src/Infrastructure/Data/Configurations/ChannelConfiguration.cs
@@ -0,0 +1,14 @@
+﻿//using SMP.Domain.Entities;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Metadata.Builders;
+//using SMP.Domain.Entities.ChannelEntities;
+
+//namespace SMP.Infrastructure.Data.Configurations;
+
+//public class ChannelConfiguration : IEntityTypeConfiguration<Channel>
+//{
+//    public void Configure(EntityTypeBuilder<Channel> builder)
+//    {
+       
+//    }
+//}
---
+++ tests/Application.FunctionalTests/TodoItems/Commands/DeleteTodoItemTests.cs
@@ -0,0 +1,41 @@
+﻿using SMP.Application.TodoItems.Commands.CreateTodoItem;
+using SMP.Application.TodoItems.Commands.DeleteTodoItem;
+using SMP.Application.TodoLists.Commands.CreateTodoList;
+using SMP.Domain.Entities;
+
+namespace SMP.Application.FunctionalTests.TodoItems.Commands;
+
+using static Testing;
+
+public class DeleteTodoItemTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldRequireValidTodoItemId()
+    {
+        var command = new DeleteTodoItemCommand(99);
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Test]
+    public async Task ShouldDeleteTodoItem()
+    {
+        var listId = await SendAsync(new CreateTodoListCommand
+        {
+            Title = "New List"
+        });
+
+        var itemId = await SendAsync(new CreateTodoItemCommand
+        {
+            ListId = listId,
+            Title = "New Item"
+        });
+
+        await SendAsync(new DeleteTodoItemCommand(itemId));
+
+        var item = await FindAsync<TodoItem>(itemId);
+
+        item.Should().BeNull();
+    }
+}
